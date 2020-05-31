4956f0d915d83a9c035d3fc18c2ba4f7f421b659
david <davdiaavi@gmail.com>
2020-05-31 16:49:49 +0200

    tests update

--- src/test/java/be/taskmanager/taskmanager/TaskServiceTests.java
+++ src/test/java/be/taskmanager/taskmanager/TaskServiceTests.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +24,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-//@DataJpaTest
+@SpringBootTest
 public class TaskServiceTests {
 
     @TestConfiguration
